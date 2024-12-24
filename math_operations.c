a21098c4415e2d761bc22341acffaf7881dcd6bf
akashgaikwad26 <122025484+akashgaikwad26@users.noreply.github.com>
2024-12-24 14:53:24 +0530

    Update math_operations.c

--- math_operations.c
+++ math_operations.c
@@ -1,9 +1,9 @@
 // math_operations.c
 #include "math_operations.h"
 
-int add(int a, int b) {
-    return a + b;
-}
+// int add(int a, int b) {
+//     return a + b;
+// }
 
 int subtract(int a, int b) {
     return a - b;
